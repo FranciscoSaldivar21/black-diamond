aee82df96bb2f9ac3700c5fa003a96943730a849
FranciscoSaldivar21 <francisco.saldivar4081@alumnos.udg.mx>
2023-09-14 19:18:14 -0600

    Api URL updated

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
-// export const apiURL = "https://black-diamond-back-production.up.railway.app/api/";
-export const apiURL = "http://localhost:3000/api/";
+// export const apiURL = "http://localhost:3000/api/";
+export const apiURL = "https://black-diamond-back-production.up.railway.app/api/";
